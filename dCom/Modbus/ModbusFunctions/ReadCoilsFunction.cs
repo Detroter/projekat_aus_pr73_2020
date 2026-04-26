a6a3fbd0d260dddcfafe812b49a01a311d196b4c
Detroter <zoojocascg@gmail.com>
2026-04-26 18:14:24 +0200

    Implementiran DIgital Output
    Napisana logika iza write sa dcom strane i read sa mdbsim strane. Uradjeno preko PackRequesta i DIctionary-a

--- dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -25,14 +25,55 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] request = new byte[12];
+
+            ModbusReadCommandParameters ModBusRead = this.CommandParameters as ModbusReadCommandParameters;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusRead.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusRead.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusRead.Length)), 0, request, 4, 2);
+
+            request[6] = ModBusRead.UnitId;
+            request[7] = ModBusRead.FunctionCode;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusRead.StartAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusRead.Quantity)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters ModBusRead = this.CommandParameters as ModbusReadCommandParameters;
+
+            Dictionary<Tuple<PointType, ushort>, ushort> dict = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            int count = 0;
+            ushort address = ModBusRead.StartAddress;
+            ushort value;
+            byte mask = 1;
+
+            for (int i = 0; i < response[8]; i++)
+            {
+                byte temp = response[9 + i];
+
+                for (int j = 0; j < 8; j++)
+                {
+                    value = (ushort)(temp & mask);
+                    temp >>= 1;
+                    dict.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, address), value);
+                    count++;
+                    address++;
+
+                    if (count == ModBusRead.Quantity)
+                    {
+                        break;
+                    }
+                }
+            }
+            return dict;
         }
     }
 }
--- dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
+++ dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
@@ -25,14 +25,36 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters ModBusWrite = this.CommandParameters as ModbusWriteCommandParameters;
+
+            byte[] request = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusWrite.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusWrite.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusWrite.Length)), 0, request, 4, 2);
+
+            request[6] = ModBusWrite.UnitId;
+            request[7] = ModBusWrite.FunctionCode;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusWrite.OutputAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusWrite.Value)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters ModBusWrite = this.CommandParameters as ModbusWriteCommandParameters;
+
+            Dictionary<Tuple<PointType, ushort>, ushort> dict = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            ushort value = (ushort)(response[11] + (response[10] << 8));
+
+            dict.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, (ushort)(ModBusWrite.OutputAddress)), value);
+
+            return dict;
         }
     }
 }
