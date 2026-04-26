c7df33577af787d547b7025466fb6a5434802e64
Detroter <zoojocascg@gmail.com>
2026-04-26 18:32:20 +0200

    Implementiran Analog Input
    Napisana logika iza citanja podataka koje salje mdbsim strana. Uradjeno preko PackRequesta i Dictionary-a, na isti nacin kao sto je bio Analog Output samo promena kod PointType-a i nema mogucnosti write-a.

--- dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
@@ -25,14 +25,46 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] request = new byte[12];
+
+            ModbusReadCommandParameters ModBusRead = this.CommandParameters as ModbusReadCommandParameters;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusRead.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusRead.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusRead.Length)), 0, request, 4, 2);
+
+            request[6] = ModBusRead.UnitId;
+            request[7] = ModBusRead.FunctionCode;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusRead.StartAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)ModBusRead.Quantity)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters ModBusRead = this.CommandParameters as ModbusReadCommandParameters;
+
+            Dictionary<Tuple<PointType, ushort>, ushort> dict = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            ushort byteCount = response[8];
+            ushort value;
+            int byte02Start = 7;
+            int byte01Start = 8;
+
+            for (int i = 0; i < byteCount / 2; i++)
+            {
+                byte secondByte = response[byte02Start += 2];
+                byte firstByte = response[byte01Start += 2];
+
+                value = (ushort)(firstByte + (secondByte << 8));
+
+                dict.Add(new Tuple<PointType, ushort>(PointType.ANALOG_INPUT, (ushort)(ModBusRead.StartAddress + i)), value);
+            }
+            return dict;
         }
     }
 }
